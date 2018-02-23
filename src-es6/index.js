cbf8f7950b6d2d57bb6424bd1a82eecca1367ab8
Till Arnold <throwable42@gmail.com>
2018-02-23 11:48:09 +0100

    Start addint layer support

--- src-es6/index.js
+++ src-es6/index.js
@@ -4,6 +4,10 @@ import {
   createCanvasEventEmitter
 } from 'canvas-utils'
 
+import {
+  LayerManager,
+  ExpanderLayer
+} from '@tillarnold/oni'
 
 const pressedMouseButtons = Object.create(null)
 
@@ -17,9 +21,68 @@ document.addEventListener('mouseup', evt => {
 
 
 module.exports = class Staffelei extends Leinwand {
-  constructor(canvas) {
+  constructor(canvas, {
+    mode = 'simple',
+    width,
+    height
+  } = {}) {
+    let container = canvas
+    if (mode !== 'layered' && mode !== 'simple') {
+      throw new Error('mode has to be simple or layered')
+    }
+
+    if (mode === 'layered') {
+      canvas = {
+        getContext: () => null
+      }
+    }
+
     super(canvas)
+
+
+    this._container = container
+
+    if (mode === 'layered') {
+      this._canvas = null
+      this._lm = new LayerManager(width, height)
+      this._lm.attachTo(this._container)
+      this._layers = new Map()
+    }
+
     this._canvasEventEmitter = null
+    this._mode = mode
+  }
+
+  createLayer(name, {
+    layer = new ExpanderLayer(),
+    position
+  } = {}) {
+    if (this._mode !== 'layered') {
+      throw new Error('This Staffelei is not in layered mode')
+    }
+
+    this._lm.add(layer, position)
+    this._layers.set(name, layer)
+    return this
+  }
+
+  layer(name) {
+    if (this._mode !== 'layered') {
+      throw new Error('This Staffelei is not in layered mode')
+    }
+
+    const layer = this._layers.get(name)
+
+    this.setCanvas(layer.getElement())
+    return this
+  }
+  /*
+   * Move to to leinwand?
+   */
+  setCanvas(canvas) {
+    this._canvas = canvas
+    this._ctx = canvas.getContext('2d')
+    return this
   }
 
   toImage() {
--- src-es6/test/beefy.js
+++ src-es6/test/beefy.js
@@ -1,17 +1,21 @@
 const Staffelei = require('../')
 
-const canvas = document.createElement('canvas')
-canvas.width = 500
-canvas.height = 500
-canvas.style.outline = '1px solid black'
-document.body.appendChild(canvas)
+const container = document.createElement('div')
+container.style.outline = '1px solid black'
+document.body.appendChild(container)
 
 
-const l = new Staffelei(canvas)
+const l = new Staffelei(container, {
+  mode: 'layered',
+  width: '250',
+  height: '250'
+})
 
 window.l = l
-
 l
+  .createLayer('background', 0)
+  .createLayer('front', 1)
+  .layer('background')
   .fillStyle('red')
   .mt(50, 50)
   .lt(250, 70)
@@ -25,13 +29,13 @@ l
   .circle(50, 50, 40)
   .fill()
   .stroke()
-  .on('click', e => {
-    l.fillStyle('lime').fillRectCenteredAt(e.x, e.y, 15, 15)
-  })
   .fillRect(200, 200, 100, 100)
   .rotateContextAt(250, 250, Math.PI / 4)
   .fillRect(200, 200, 100, 100)
   .resetTransforms()
+  .on('click', e => {
+    l.layer('front').fillStyle('lime').fillRectCenteredAt(e.x, e.y, 15, 15)
+  })
 
 
 setInterval(() => {
