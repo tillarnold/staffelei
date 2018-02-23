ce4cc9b3f7fce3338e6230e6d17c0b35e7168f63
Till Arnold <throwable42@gmail.com>
2018-02-23 21:13:00 +0100

    Append version to generated js files

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 export default {
   input: 'src-es6/index.js',
   output: {
-    file: 'dist/staffelei-rollup.js',
+    file: 'dist/staffelei-rollup-v' + process.env.npm_package_version + '.js',
     format: 'umd',
     name: 'Staffelei'
   },
